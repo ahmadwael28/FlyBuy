87aea86bbcf22cd81a69eb8bfd19ce50bb887e00
Ahmad Wael <Ahmad Wael@DESKTOP-1GT37LU>
2020-05-04 21:46:22 +0200

    finalized pagination

--- src/app/Components/pagination/pagination.component.ts
+++ src/app/Components/pagination/pagination.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input, SimpleChange, Output, EventEmitter } from '@a
 export class PaginationComponent implements OnInit {
 
   nPages;
+  CurrentPage = 1;
   constructor() { 
     
   }
@@ -21,13 +22,59 @@ export class PaginationComponent implements OnInit {
   {
     console.log(this.Npages);
     this.nPages = Array(this.Npages).fill(this.Npages);
+    this.UpdateProductsSectionManually(1);
+    this.UpdateButtons();
   }
+
+
+  Previous()
+  {
+    if(this.CurrentPage > 1)
+    {
+      this.CurrentPage--;
+    this.UpdateButtons();
+    this.RequestPage.emit(this.CurrentPage);
+    }
+  }
+
+  Next()
+  {
+    if(this.CurrentPage < this.nPages.length)
+    {
+      this.CurrentPage++;
+      this.UpdateButtons();
+      this.RequestPage.emit(this.CurrentPage);
+    }
+  }
+
+  UpdateButtons()
+  {
+    if(this.CurrentPage == 1)
+    {
+      document.getElementById("prev").classList.add("disabled");
+      document.getElementById("next").classList.remove("disabled");
+    }
+    else if(this.CurrentPage == this.nPages.length)
+    {
+      document.getElementById("next").classList.add("disabled");
+      document.getElementById("prev").classList.remove("disabled");
+    }
+  }
+
 
   @Output() RequestPage = new EventEmitter<number>();
 
   UpdateProductsSection(event): void {
     var selectedPage = event.target.innerText;
-    this.RequestPage.emit(selectedPage);
+    this.CurrentPage = Number.parseInt(selectedPage);
+    this.RequestPage.emit(this.CurrentPage);
+    this.UpdateButtons();
+  }
+
+  UpdateProductsSectionManually(pageno): void {
+    this.CurrentPage = Number.parseInt(pageno);
+    this.RequestPage.emit(this.CurrentPage);
+    this.UpdateButtons();
   }
 
 }
