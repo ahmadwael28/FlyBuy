9b668ec550cecd69415b9ccaa10214abe041bd9a
mai zakaria <mai.zakaria.stdsw@cis.asu.edu.eg>
2020-05-12 21:17:36 +0200

    -Minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component ,OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './shared/auth.service';
 import { ShoppingCartService } from 'src/app/Service/shopping-cart.service';
-import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +10,8 @@ import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.
 })
 export class AppComponent implements OnInit{
   title = 'Fly-Buy';
-<<<<<<< HEAD
   cartLength;
-  constructor(public authService: AuthService, private router: Router, private shoppingCartService:ShoppingCartService,
-    private shoppingCart:ShoppingCartComponent) { 
+  constructor(public authService: AuthService, private router: Router, private shoppingCartService:ShoppingCartService) { 
     
   }
   ngOnInit(): void {
@@ -32,9 +29,6 @@ export class AppComponent implements OnInit{
   logout() {
     this.authService.doLogout();
     this.router.navigate(['Login']);
-=======
-  constructor(public authService: AuthService, private router: Router) {
->>>>>>> 817e55eec32718e21bbb271600ada5302145be14
   }
 
   
