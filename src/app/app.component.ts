c2636160645f13d476de297862f14ed6999a493d
mai zakaria <mai.zakaria.stdsw@cis.asu.edu.eg>
2020-05-12 02:54:07 +0200

    -Added cart length in appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './shared/auth.service';
+import { ShoppingCartService } from 'src/app/Service/shopping-cart.service';
+import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Fly-Buy';
-  constructor(public authService: AuthService, private router: Router) { }
+  cartLength;
+  constructor(public authService: AuthService, private router: Router, private shoppingCartService:ShoppingCartService,
+    private shoppingCart:ShoppingCartComponent) { 
+    
+  }
+  ngOnInit(): void {
+ 
+    let getUserShoppingCartObservable = this.shoppingCartService.getUserShoppingCart();
+    let getUserShoppingCartDispose = getUserShoppingCartObservable.subscribe((data:any) => {
+      this.cartLength = data.ShoppingCart.Products.length;
+    getUserShoppingCartDispose.unsubscribe();
+    },
+    (err)=>{
+      console.log(err);
+    });
+  }
 
   logout() {
     this.authService.doLogout();
